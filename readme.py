339d77d1a0287f6cbbb828d8536c7146f62cc1c7
2120191 <jorgeluisromeromejia4@gmail.com>
2024-05-10 08:29:18 -0500

    morat

---
+++ readme.py
@@ -0,0 +1,45 @@
+## ejercicios
+
+edad=print(int(input("ingresar edad"))
+           if edad <=17:
+           print("eres menor")
+           if edad >= 18:
+           print("eres mayor")
+
+## ejercicio dos 
+
+``` python
+# Almacenar la contraseña en una variable
+contrasena_almacenada = "contrasena123"
+
+# Solicitar al usuario que ingrese la contraseña
+contrasena_usuario = input("Por favor, ingrese la contraseña: ")
+
+# Verificar si la contraseña ingresada coincide con la contraseña almacenada
+if contrasena_usuario == contrasena_almacenada:
+    print("¡La contraseña es correcta!")
+else:
+    print("La contraseña es incorrecta. Inténtalo de nuevo.")
+
+
+
+contraseña_="123456"
+print()
+contraseña=("ingresar contraseña")
+print = contraseña_ == contraseña
+
+
+##ejercicio tres
+
+``` python
+numero=int(input("ingrese numero entero positivo"))
+numero = int(input("Por favor, ingrese un número entero positivo: "))
+if numero < 0:
+    print("El número ingresado no es positivo. Por favor, ingrese un número entero positivo.")
+else:
+ cuenta_regresiva = list(range(numero, -1, -1))
+    cuenta_regresiva_str = ', '.((str, cuenta_regresiva))
+    print(f"Cuenta regresiva desde {numero} hasta 0: {cuenta_regresiva_str}")
+
+##
+``` python
