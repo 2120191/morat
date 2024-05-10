d46b12f63abc4a84761b3dd7eb66cda311819813
2120191 <jorgeluisromeromejia4@gmail.com>
2024-05-10 08:55:16 -0500

    actualise la carpeta morat

--- readme.py
+++ readme.py
@@ -45,3 +45,20 @@ else:
 ``` python
 ## concepto
 ¿Qué son los algoritmos? En informática, se llaman algoritmos el conjunto de instrucciones sistemáticas y previamente definidas que se utilizan para realizar una determinada tarea. Estas instrucciones están ordenadas y acotadas a manera de pasos a seguir para alcanzar un objetivo.
+Algoritmos en nuestras vidas
+Si miramos la definición de la Real Academia Española, nos dice que la definición de algoritmo es: “Conjunto ordenado y finito de operaciones que permite hallar la solución de un problema”.
+
+Si bien los algoritmos generalmente se asocian al ámbito matemático, no necesariamente implica que sean exclusivos de esta área. Se puede entender un algoritmo como una secuencia de pasos finitos bien definidos que resuelven un problema. Por ejemplo, la ejecución de tareas cotidianas tan simples como cepillarse los dientes, lavarse las manos o seguir el manual de instrucciones de armado de un mueble, se pueden ver como un algoritmo.
+
+¿Qué son los algoritmos? ¿Cuál es el concepto de algoritmo? ¿Qué es un algoritmo en programación? Desde el punto de vista informático un algoritmo es cualquier procedimiento computacional bien definido que parte de un estado inicial y un valor o un conjunto de valores de entrada, a los cuales se les aplica una secuencia de pasos computacionales finitos, produciendo una salida o solución. Se puede considerar al algoritmo como una herramienta para resolver un cálculo computacional bien especificado [1].
+
+¿Qué tan recientes son los algoritmos?
+Los humanos han creado, modificado y utilizado algoritmos a lo largo de la historia. Hace más de 4500 años los sumerios ilustraron en tablas de arcilla un método repetitivo utilizado para distribuir de forma equitativa la cosecha de granos entre un número variable de hombres [2].
+
+Aproximadamente en el año 300 A.C en la Grecia Helenística, el matemático Euclides escribió “Elementos”, un trabajo que sentaría las bases para la geometría de los próximos milenios, ese texto incluye su famoso algoritmo para hallar el máximo común divisor.
+
+A mediados del siglo XIX, Augusta Ada Byron (Ada Lovelace) crea lo que se considera el primer algoritmo diseñado para ser ejecutado por una máquina. Lovelace, sentó las bases para los lenguajes de programación futuros (como la programación funcional).
+
+Ya en el siglo pasado con la llamada “Revolución Informática” los algoritmos comenzaron a tener un peso cada vez mayor. A medida que avanzaba la era de la computación estos adquirieron cada vez más relevancia hasta convertirse en parte de un engranaje vital en los procesos tecnológicos que afectan nuestro diario vivir.
+
+ 
